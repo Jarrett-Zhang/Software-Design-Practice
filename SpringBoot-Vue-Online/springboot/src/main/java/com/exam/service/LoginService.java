6b75236688969c2485b21a6848f2c7b3cd699354
YeMo0001 <163085501+YeMo0001@users.noreply.github.com>
2024-06-16 18:48:03 +0800

    Add files via upload

---
+++ SpringBoot-Vue-Online/springboot/src/main/java/com/exam/service/LoginService.java
@@ -0,0 +1,16 @@
+package com.exam.service;
+
+import com.exam.entity.Admin;
+import com.exam.entity.Student;
+import com.exam.entity.Teacher;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface LoginService {
+
+    public Admin adminLogin(Integer username, String password);
+
+    public Teacher teacherLogin(Integer username, String password);
+
+    public Student studentLogin(Integer username, String password);
+}
